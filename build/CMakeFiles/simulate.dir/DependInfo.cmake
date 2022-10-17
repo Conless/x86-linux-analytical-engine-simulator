062d8e3ce60c26aa5d10879f7df680fc9bd7cb42
Conless Pan <conlesspan@outlook.com>
2022-10-17 13:40:51 +0000

    commit

--- build/CMakeFiles/simulate.dir/DependInfo.cmake
+++ build/CMakeFiles/simulate.dir/DependInfo.cmake
@@ -1,14 +1,15 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/x86-linux-analytical-engine-simulator/main.cpp" "/workspaces/x86-linux-analytical-engine-simulator/build/CMakeFiles/simulate.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/mnt/d/OneDrive/cs/course/22fall/sjtu/cs1950/week_2/x86-linux-analytical-engine-simulator/main.cpp" "CMakeFiles/simulate.dir/main.cpp.o" "gcc" "CMakeFiles/simulate.dir/main.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
   )
 
 # Targets to which this target links.
--- build/CMakeFiles/simulate.dir/cmake_clean.cmake
+++ build/CMakeFiles/simulate.dir/cmake_clean.cmake
@@ -1,6 +1,5 @@
 file(REMOVE_RECURSE
   "CMakeFiles/simulate.dir/main.cpp.o"
-  "CMakeFiles/simulate.dir/main.cpp.o.d"
   "simulate"
   "simulate.pdb"
 )
